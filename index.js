25fdb0c3a022159c5fefdad83b7a196590d8f7d0
svenl <sven.lecann@live.fr>
2018-11-20 17:21:45 +0100

    test sequalize

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
-var http = require('http');
 
-var server = http.createServer(function(request, response) {
 
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World!");
-
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize('MEGACASTING', 'adminmegacasting', 't4tX38CwrHQJbDWkl2qr', {
+  host: 'megacasting.database.windows.net',
+  dialect: 'mssql',
+  dialectOptions: {
+    encrypt: true
+  }
 });
 
-var port = process.env.PORT || 1337;
-server.listen(port);
-
-console.log("Server running at http://localhost:%d", port);
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });

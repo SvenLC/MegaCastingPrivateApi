1cd9254da79b02b6d5ea82ffb0dd426a4e6c00ab
svenl <sven.lecann@live.fr>
2018-11-21 16:06:23 +0100

    test api

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 const http = require('http');
 const port=process.env.PORT || 3000
-const server = http.createServer((req, res) => {
-res.statusCode = 200;
-res.setHeader('Content-Type', 'text/html');
-res.end('<h1>Hello World</h1>');
+var express = require('express');
+var app = express();
+
+app.get("/utilisateur",function(request,response)
+{
+    response.json({"Message":"Retourne les utilisateurs"});
 });
-server.listen(port,() => {
-console.log(`Server running at port `+port);
+
+app.route('/').get(function(req, res) { 
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.end('<h1>API MegaCasting</h1>'); 
+}); 
+ 
+app.listen(port, function () {
+    var datetime = new Date();
+    var message = "Server runnning on Port:- " + port + "Started at :- " + datetime;
+    console.log(message);
 });

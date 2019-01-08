95496beb5a4668303fa04db5c5f4f7610af0d102
svenl <sven.lecann@live.fr>
2019-01-08 10:25:44 +0100

    Issue utilisateur route

--- routes/utilisateur.js
+++ routes/utilisateur.js
@@ -1,5 +1,7 @@
 const express = require('express');
-const { body } = require('express-validator/check');
+const {
+    body
+} = require('express-validator/check');
 
 const utilisateurController = require('../controllers/utilisateur');
 
@@ -9,35 +11,44 @@ router.get('/:utilisateurId', utilisateurController.getUtilisateur);
 router.get('/', utilisateurController.getUtilisateurs);
 router.post('/', [
     body('UTI_MDP')
-        .trim()
-        .isLength({ min: 6 }),
+    .trim()
+    .isLength({
+        min: 6
+    }),
     body('UTI_LOGIN')
-        .trim()
-        .not()
-        .isEmpty()
-        .isLength({ min: 4 })
+    .trim()
+    .not()
+    .isEmpty()
+    .isLength({
+        min: 4
+    })
 ], utilisateurController.createUtilisateur);
 
 router.put('/:utilisateurId', [
     body('UTI_MDP')
-        .trim()
-        .isLength({ min: 6 }),
+    .trim()
+    .isLength({
+        min: 6
+    }),
     body('UTI_LOGIN')
-        .trim()
-        .not()
-        .isEmpty()
-        .isLength({ min: 4 })
+    .trim()
+    .not()
+    .isEmpty()
+    .isLength({
+        min: 4
+    })
 ], utilisateurController.updateUtilisateur);
 
-router.put('/mdp/:utilisateurId', [
-    body('UTI_MDP')
-    .trim()
-    .isLenght({ min: 6})
-], utilisateurController.updateUtilisateurMdp);
+router.put('/mdp/:utilisateurId' 
+// [
+//     body('UTI_MDP')
+//     .trim()
+//     .isLenght({
+//         min: 6
+//     })
+// ]
+, utilisateurController.updateUtilisateurMdp);
 
 router.delete('/:utilisateurId', utilisateurController.deleteUtilisateur);
 
-
-
 module.exports = router;
-

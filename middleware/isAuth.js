94ae5e61ad0b42b59dfd6f8db5667e87b585f2b6
svenl <sven.lecann@live.fr>
2019-01-09 10:35:34 +0100

    auth test

--- middleware/isAuth.js
+++ middleware/isAuth.js
@@ -2,25 +2,35 @@ const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
     const autHeader = req.get('Authorization');
-    if (!autHeader) {
-        const error = new Error('Non authentifié');
-        error.statusCode = 401;
-        throw error;
-    }
-    const token = autHeader.split(' ')[1];
-    let decodedToken;
-    try {
-        decodedToken = jwt.verify(token, 'BDB971EA6E6788317F359F23E86C5');
-    } catch (error) {
-        error.statusCode = 500;
-        throw error;
-    }
-    if (!decodedToken) {
-        const error = new Error('Non authentifié');
-        error.statusCode = 401;
-        throw error;
-    }
-    req.userId = decodedToken.userId;
-    next();
-};
+    const domain = 'https://megacastingpublicapi.azurewebsites.net';
+    const local = 'http://localhost:3000';
+    const url = req.get('Origin');
 
+    if (url == domain || url == local) {
+
+        next();
+
+    } else {
+        const autHeader = req.get('Authorization');
+        if (!autHeader) {
+            const error = new Error('Non authentifié');
+            error.statusCode = 401;
+            throw error;
+        }
+        const token = autHeader.split(' ')[1];
+        let decodedToken;
+        try {
+            decodedToken = jwt.verify(token, 'BDB971EA6E6788317F359F23E86C5');
+        } catch (error) {
+            error.statusCode = 500;
+            throw error;
+        }
+        if (!decodedToken) {
+            const error = new Error('Non authentifié');
+            error.statusCode = 401;
+            throw error;
+        }
+        req.userId = decodedToken.userId;
+        next();
+    }
+};

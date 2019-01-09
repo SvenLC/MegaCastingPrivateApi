7f41149f3f19f43682d2cdce1e127bc2c6b1bd58
svenl <sven.lecann@live.fr>
2019-01-09 11:12:01 +0100

    change routing order for prospect

--- routes/prospect.js
+++ routes/prospect.js
@@ -4,9 +4,9 @@ const prospectController = require('../controllers/prospect');
 
 const router = express.Router();
 
-router.get('/:prospectId', prospectController.getProspect);
 router.get('/', prospectController.getProspects);
 router.get('/formated', prospectController.getProspectFormated);
+router.get('/:prospectId', prospectController.getProspect);
 router.post('/', prospectController.createProspect);
 router.delete('/:prospectId', prospectController.deleteProspect);
 router.put('/:prospectId', prospectController.updateProspect);
